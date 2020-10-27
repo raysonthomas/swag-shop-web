937d2ecfb4e9649fac48e4ccd306dd4564e4b7cf
Rayson <rayson.thomas@gmail.com>
2020-10-27 09:53:28 +0000

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App/App.js';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css'; // eslint-disable-next-line
+import $ from 'jquery';// eslint-disable-next-line
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.render(
   <React.StrictMode>
